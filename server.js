ae91846c034df986a316de6583de09765169c793
simonyan1 <varazdat.simonyan@tumo.org>
2024-01-21 15:25:25 +0400

    DB

--- server.js
+++ server.js
@@ -1,24 +1,48 @@
-var express = require("express");
-var path = require("path");
-const bodyParser = require('body-parser');
-var app = express();
+// var express = require("express");
+// var path = require("path");
+// const bodyParser = require('body-parser');
+// var app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname,'./public/form.html'));
-});
+// app.get("/", function(req, res){
+//     res.sendFile(path.join(__dirname,'./public/form.html'));
+// });
 
-app.post('/addName', (req, res) => {
-    const name = req.body.name;
-    const age = req.body.age;
-    console.log('Received data 1:', name , age );
-    res.redirect('/');
- });
+// app.post('/addName', (req, res) => {
+//     const name = req.body.name;
+//     const age = req.body.age;
+//     console.log('Received data 1:', name , age );
+//     res.redirect('/');
+//  });
 
-app.listen(3000, function(){
-   console.log("Example is running on port 3000");
-});
+// app.listen(3000, function(){
+//    console.log("Example is running on port 3000");
+// });
+
+
+const mongoose = require('mongoose');
+
+const connectionString = 'mongodb+srv://Tumo:Tumo1234@cluster0.ek0rehr.mongodb.net/sample_mflix';
+
+
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', async () => {
+    console.log('Connected to MongoDB!');
+
+    try {
+        await mongoose.connection.db.collection('sessions').insertOne({
+            user_id: 1,
+            name:'asd'
+        })
+    } catch (error) {
+        console.error('Error retrieving movies:', error);
+    } finally {
+        mongoose.connection.close();
+    }
+})
