0f63260d71f7f473f4281355102004738c8cb7b5
simonyan1 <varazdat.simonyan@tumo.org>
2024-01-26 21:24:06 +0400

    take info from db

--- server.js
+++ server.js
@@ -6,15 +6,32 @@ var app = express();
 const mongoose = require('mongoose');
 const connectionString = 'mongodb+srv://Tumo:Tumo1234@cluster0.ek0rehr.mongodb.net/sample_mflix';
 
-
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static('public'));
 
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname,'./public/form.html'));
+app.get("/", function (req, res) {
+    mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'Connection error:'));
+    db.once('open', async () => {
+        try {
+            let result = await mongoose.connection.db.collection('theaters').find({'location.address.city':'Bloomington'}).toArray()
+            res.render('../public/form.ejs', {
+                obj: result
+            });
+        } catch (error) {
+            console.error('Error retrieving movies:', error);
+        } finally {
+            mongoose.connection.close();
+        }
+    })
+    // var info = [
+    //     { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012 },
+    //     { name: 'Tux', organization: "Linux", birth_year: 1996 },
+    //     { name: 'Moby Dock', organization: "Docker", birth_year: 2013 }
+    // ];
 });
 
 app.post('/addName', async (req, res) => {
@@ -27,7 +44,7 @@ app.post('/addName', async (req, res) => {
     db.once('open', async () => {
         console.log('Connected to MongoDB!');
         try {
-           let result = await mongoose.connection.db.collection('users').insertOne({
+            let result = await mongoose.connection.db.collection('users').insertOne({
                 name: name,
                 email: email,
                 password: password
@@ -39,11 +56,11 @@ app.post('/addName', async (req, res) => {
             mongoose.connection.close();
         }
     })
- });
+});
 
 
 
 
-app.listen(3000, function(){
-   console.log("Example is running on port 3000");
+app.listen(3000, function () {
+    console.log("Example is running on port 3000");
 });
